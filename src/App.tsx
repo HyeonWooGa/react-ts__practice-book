c17b31c74ea4117a617375d536bbc3e21d5ebded
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-02 10:47:20 +0900

    onClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRecoilState } from "recoil";
 import { nameState } from "./atoms";
 
 function App() {
   const [name, setName] = useRecoilState(nameState);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
     // console.log(e.target.value);
+  };
+
+  const handleClick = (event: React.FormEvent<HTMLButtonElement>) => {
+    alert(name);
   };
 
   return (
     <>
       <input type="text" value={name} onChange={handleChange}></input>
+      <button onClick={handleClick}>Button</button>
       <h1>{name}</h1>
     </>
   );
