22641f705cc5f5a8f0032a0ebe04eebc71ac13e6
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-02 10:32:38 +0900

    onChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <></>;
+  const [name, setName] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  return (
+    <>
+      <input type="text" value={name} onChange={handleChange}></input>
+      <h1>{name}</h1>
+    </>
+  );
 }
 
 export default App;
